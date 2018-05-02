4765be8a177a79c55c833d362644f99560939211
Brian Okken <1568356+okken@users.noreply.github.com>
2018-05-02 15:12:36 -0700

    Make the cli tests more readable

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -7,12 +7,12 @@ Not in depth tests of functionality.
 
 import pytest
 import cards.cli
+from cards import Card
 from . import detabulate_output
 
-pytestmark = pytest.mark.cli
+pytestmark = [pytest.mark.cli, pytest.mark.smoke]
 
 
-@pytest.mark.smoke
 def test_add(db_empty, cards_cli):
     # GIVEN an empty database
     # WHEN a new card is added
@@ -25,7 +25,6 @@ def test_add(db_empty, cards_cli):
     assert items[0][3] == 'something'
 
 
-@pytest.mark.smoke
 def test_list(db_empty, cards_cli):
     # GIVEN a db with known contents of 2 cards
     cards_cli('add one')
@@ -41,7 +40,6 @@ def test_list(db_empty, cards_cli):
     assert items[1][3] == 'two'
 
 
-@pytest.mark.smoke
 def test_list_filter(db_empty, cards_cli):
     # GIVEN
     #  two items owned by okken, one that is done
@@ -61,7 +59,6 @@ def test_list_filter(db_empty, cards_cli):
             ['4', ''     , 'x', 'four' ]] == items  # noqa:E202,E203
 
 
-@pytest.mark.smoke
 def test_count(db_empty, runner):
     # GIVEN a db with 2 cards
     runner.invoke(cards.cli.cards_cli, ['add', 'one'])
@@ -73,40 +70,50 @@ def test_count(db_empty, runner):
     assert expected == result.output
 
 
-# this test was annoying to write. parsing list output is yucky
-@pytest.mark.smoke
-def test_update(db_non_empty, runner):
-    # GIVEN a card known to be in the db
-    result = runner.invoke(cards.cli.cards_cli, ['list', '--format=jira'])
+def packed_line_to_card(line):
+    card_id, owner, done, summary = line.split(maxsplit=3)
+    return Card(summary, owner, done == 'x', int(card_id))
 
-    _, orig_items = detabulate_output(result.output)
+
+def packed_output_to_cards(list_output):
+    return [packed_line_to_card(line) for line in list_output.split('\n')]
+
+
+@pytest.fixture()
+def db_non_empty_no_okken_no_done(db_non_empty, cards_cli):
+    output = cards_cli('list --format=packed')
+    for card in packed_output_to_cards(output):
+        assert card.owner != 'okken'
+        assert not card.done
+
+
+# this test would be easier if we add an id search parameter to list
+def test_update(db_non_empty_no_okken_no_done, cards_cli):
+    # GIVEN a card known to be in the db
+    output = cards_cli('list --format=packed')
+    old_cards = packed_output_to_cards(output)
 
     # WHEN we `cards update` the card with new info
-    runner.invoke(cards.cli.cards_cli,
-                  ['update', '-o', 'okken', '-d', 'True', orig_items[1][0]])
+    cards_cli(f'update -o okken -d True {old_cards[1].id}')
 
     # THEN `cards list` will show the changes
-    result = runner.invoke(cards.cli.cards_cli, ['list', '--format=jira'])
-    _, new_items = detabulate_output(result.output)
-    assert orig_items[1][0] == new_items[1][0]
-    assert new_items[1][1] == 'okken'
-    assert new_items[1][2] == 'x'
-    assert orig_items[1][3] == new_items[1][3]
+    new_output = cards_cli('list --format=packed')
+    new_cards = packed_output_to_cards(new_output)
+
+    assert old_cards[1].id == new_cards[1].id
+    assert 'okken' == new_cards[1].owner
+    assert new_cards[1].done
 
 
-@pytest.mark.smoke
-def test_delete(db_empty, runner):
+def test_delete(db_empty, cards_cli):
     # GIVEN a db with 2 items
-    runner.invoke(cards.cli.cards_cli, ['add', 'one'])
-    runner.invoke(cards.cli.cards_cli, ['add', 'two'])
+    cards_cli('add one')
+    cards_cli('add two')
 
     # WHEN we delete one item
     # hoping ids always start with 1
-    runner.invoke(cards.cli.cards_cli, ['delete', '1'])
+    cards_cli('delete 1')
 
     # THEN the other card remains in the db
-    result = runner.invoke(cards.cli.cards_cli, ['list', '--format=jira'])
-    headers, items = detabulate_output(result.output)
-    assert headers == ['ID', 'owner', 'done', 'summary']
-    assert items[0][0] == '2'
-    assert items[0][3] == 'two'
+    output = cards_cli('list --format=packed')
+    assert '2 unassigned o two' == output
